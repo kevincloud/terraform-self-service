4592fa7a4338c4a4c715a6f129bf635545fc7352
Kevin Cochran <kcochran@hashicorp.com>
2020-08-09 11:47:01 -0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,70 @@
+provider "tfe" { }
+
+resource "random_integer" "wkspid" {
+    keepers = {
+        uuid = uuid()
+    }
+    min = 100000
+    max = 199999
+}
+
+resource "tfe_workspace" "workspace" {
+    name = "HC${wkspid.result}"
+    organization = var.organization
+    auto_apply = true
+    vcs_repo {
+        identifier = "kevincloud/terraform-simple-instance"
+        oauth_token_id = var.vcs_token
+    }
+}
+
+resource "tfe_variable" "aws_access_key" {
+    key = "aws_access_key"
+    value = var.aws_access_key
+    category = "terraform"
+    workspace_id = tfe_workspace.workspace.id
+}
+
+resource "tfe_variable" "aws_secret_key" {
+    key = "aws_secret_key"
+    value = var.aws_secret_key
+    category = "terraform"
+    sensitive = true
+    workspace_id = tfe_workspace.workspace.id
+}
+
+resource "tfe_variable" "aws_session_token" {
+    key = "aws_session_token"
+    value = var.aws_session_token
+    category = "terraform"
+    sensitive = true
+    workspace_id = tfe_workspace.workspace.id
+}
+
+resource "tfe_variable" "aws_region" {
+    key = "aws_region"
+    value = var.aws_region
+    category = "terraform"
+    workspace_id = tfe_workspace.workspace.id
+}
+
+resource "tfe_variable" "key_pair" {
+    key = "key_pair"
+    value = var.key_pair
+    category = "terraform"
+    workspace_id = tfe_workspace.workspace.id
+}
+
+resource "tfe_variable" "instance_type" {
+    key = "instance_type"
+    value = "t3.micro"
+    category = "terraform"
+    workspace_id = tfe_workspace.workspace.id
+}
+
+resource "tfe_variable" "aws_access_key" {
+    key = "aws_access_key"
+    value = var.aws_access_key
+    category = "terraform"
+    workspace_id = tfe_workspace.workspace.id
+}
---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "api_token" {
+    description = "API Token used for provisioning"
+}
+
+variable "organization" {
+    description = "Name of the TF organization to provision in"
+}
+
+variable "vcs_token" {
+    description = "The OAuth token needed to access the repo"
+}
+
+variable "owner" {
+    description = ""
+}
+
+variable "hc_region" {
+    description = ""
+}
+
+variable "purpose" {
+    description = ""
+}
+
+variable "ttl" {
+    description = ""
+}
